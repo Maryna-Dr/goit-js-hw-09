334ebd08b2ff1c1683f6ed6e74543b1927d0e7c0
Maryna-Dr <108903370+Maryna-Dr@users.noreply.github.com>
2022-09-28 23:45:52 +0000

    Deploying to gh-pages from @ Maryna-Dr/goit-js-hw-09@89a5bdf9b213710c5419b9e0144d7307437e8eaf 🚀

---
+++ 01-color-switcher.175c619a.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]")};let n=null;function o(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}function e(){t.body.style.backgroundColor=o()}t.btnStart.addEventListener("click",(function(){t.body.style.backgroundColor=o(),n=setInterval(e,1e3),t.btnStart.disabled=!0})),t.btnStop.addEventListener("click",(function(){t.btnStart.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.175c619a.js.map
