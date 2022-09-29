43572a2b0c53bb2b69ab2f49581feadaa6157e36
Maryna-Dr <drapata.mb@gmail.com>
2022-09-30 01:09:15 +0300

    Task 2 completed

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,20 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const refs = {
+  picker: document.querySelector('#datetime-picker'),
+  btnStart: document.querySelector('button[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+const DELAY = 1000;
+let definedTime = null;
+let intervalId = null;
 
 const options = {
   enableTime: true,
@@ -7,31 +22,24 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
     countTime(selectedDates[0]);
   },
 };
 
-const refs = {
-  picker: document.querySelector('#datetime-picker'),
-  btnStart: document.querySelector('button[data-start]'),
-};
+flatpickr(refs.picker, options);
+initBtnState();
 
-let definedTime = null;
+refs.btnStart.addEventListener('click', onBtnStartClick);
 
-addFlatpick();
-changeBtnState();
-
-function addFlatpick() {
-  const fp = flatpickr(refs.picker, options);
-  refs.picker.value = '';
+function onBtnStartClick() {
+  intervalId = setInterval(ticTimer, DELAY);
 }
 
 function countTime(selectDate) {
   const currentDate = options.defaultDate;
 
   if (selectDate <= currentDate) {
-    alert('Please choose a date in the future');
+    Notify.failure('Please choose a date in the future');
     return;
   }
 
@@ -39,13 +47,53 @@ function countTime(selectDate) {
   refs.btnStart.disabled = false;
 }
 
-function changeBtnState() {
+function ticTimer() {
+  if (definedTime === null) return;
+  definedTime -= DELAY;
+
+  if (definedTime <= 0) {
+    clearInterval(intervalId);
+    return;
+  }
+
+  updateTimer(convertMs(definedTime));
+}
+
+function updateTimer(dateTime) {
+  const { days, hours, minutes, seconds } = dateTime;
+
+  refs.days.textContent = addLeadingZero(days);
+  refs.hours.textContent = addLeadingZero(hours);
+  refs.minutes.textContent = addLeadingZero(minutes);
+  refs.seconds.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function initBtnState() {
   if (definedTime === null || definedTime <= 0) {
     refs.btnStart.disabled = true;
     return;
   }
 }
 
-// дізнатися як очитити інпут після алерту, коли вибрана дата в минулому
-// з кнопками розібралася, різницю в часі рахує
-// додати конвертор для мілісекунд в нормальний формат
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
